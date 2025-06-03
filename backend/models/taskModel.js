f66895c59e0ff870a1fba0e80dd2b34d01d03357
Darkone86 <robertr106@nycstudents.net>
2025-06-03 12:50:49 -0400

    Update taskModel.js

--- backend/models/taskModel.js
+++ backend/models/taskModel.js
@@ -12,7 +12,7 @@ const getTasks = async () => {
 //Write a comment describing what this function insersts a new task into the database
 const addTask = async (title, description) => {
   const res = await db.query(
-  'INSERT INTO tasks(title, description, is_complete, created_at) VALUES ($1, $2, false, NOW()) RETURNING *',
+  "INSERT INTO tasks(title, description, is_complete, created_at) VALUES ($1, $2, false, NOW()) RETURNING *",
     [title, description]
   );
   return res.rows[0];
